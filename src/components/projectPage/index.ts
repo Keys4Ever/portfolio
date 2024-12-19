677f32dc4f49b53b59f4a3c4505a635df5050a52
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-19 17:44:20 -0300

    Feat: Implement project page components including header, content, gallery, and links

---
+++ src/components/projectPage/index.ts
@@ -0,0 +1,4 @@
+export { default as ProjectHeader } from './ProjectHeader.astro';
+export { default as ProjectContent } from './ProjectContent.astro';
+export { default as ProjectGallery } from './ProjectGallery.astro';
+export { default as ProjectLinks } from './ProjectLinks.astro';

365c35fd6028911714315b6f82dc041724dba7e0
Keys4Ever <cubillagonzalo@outlook.com>
2024-12-20 16:02:11 -0300

    Feat: Create skill categories data structure and integrate into Skills component

---
+++ src/data/skillsCats.ts
@@ -0,0 +1,63 @@
+import { Cloud, Database, Github, Layout, Palette, Server, Wrench, Java, Js, TypeScript, css, cpp, Python, NodeJS, Express, React, Astro, Sfml, Postgres, MySql, Sqlite, supabase, Aws, Figma, Photoshop, Inkscape, git, Postman, vsCode, nginx } from 'icons';
+export const skillCategories = {
+    frontend: {
+      title: 'LANGUAGES',
+      icon: Layout,
+      skills: [
+        { name: 'Java', icon: Java },
+        { name: 'TypeScript', icon: TypeScript },
+        { name: 'CSS', icon: css },
+        { name: 'C++', icon: cpp },
+        { name: 'JavaScript', icon: Js },
+        { name: 'Python', icon: Python },
+      ]
+    },
+    backend: {
+      title: 'FRAMEWORKS & LIBRARIES',
+      icon: Server,
+      skills: [
+        { name: 'Node.Js', icon: NodeJS },
+        { name: 'Express', icon: Express },
+        { name: 'React', icon: React },
+        { name: 'Astro', icon: Astro },
+        { name: 'SFML', icon: Sfml }
+      ]
+    },
+    databases: {
+      title: 'DATABASES',
+      icon: Database,
+      skills: [
+        { name: 'PostgreSQL', icon: Postgres },
+        { name: 'Sqlite', icon: Sqlite },
+        { name: 'MySQL', icon: MySql },
+        { name: 'SUPABASE', icon: supabase }
+      ]
+    },
+    cloud: {
+      title: 'CLOUD & DEVOPS',
+      icon: Cloud,
+      skills: [
+        { name: 'AWS', icon: Aws },
+      ]
+    },
+    design: {
+      title: 'DESIGN',
+      icon: Palette,
+      skills: [
+        { name: 'FIGMA', icon: Figma },
+        { name: 'PHOTOSHOP', icon: Photoshop },
+        { name: 'Inkscape', icon: Inkscape }
+    ]
+    },
+    tools: {
+      title: 'TOOLS',
+      icon: Wrench,
+      skills: [
+        { name: 'git', icon: git },
+        { name: 'Github', icon: Github },
+        { name: 'POSTMAN', icon: Postman },
+        { name: 'VS Code', icon: vsCode },
+        { name: 'NGINX', icon: nginx },
+      ]
+    }
+  };
